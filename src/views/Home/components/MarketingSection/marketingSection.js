817db18a397534a781dda1388eb9ee96ff61d3f2
Abid Anjum Khan <niloy.xponent@gmail.com>
2022-06-25 13:56:19 +0600

    single page view starting

--- src/views/Home/components/MarketingSection/marketingSection.js
+++ src/views/Home/components/MarketingSection/marketingSection.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function MarketingSection({ marketingDatas }) {
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 pt-16 lg:px-24">
@@ -16,29 +18,31 @@ export default function MarketingSection({ marketingDatas }) {
           </h3>
           <div className="grid sm:grid-cols-3 flex-[3] gap-6 ">
             {marketingData.sections.map((section, i) => (
-              <div className="border border-gra rounded-lg flex flex-col cursor-pointer">
-                <div
-                  key={i}
-                  className="text-center bg-gray-100 h-32 rounded-t-lg"
-                >
-                  <img
-                    src="/img/Home/Marketing/Page/HeroBackground/HeroBG.png"
-                    alt="hyaroo"
-                    width="100%"
-                    height="100%"
-                    className="rounded-md"
-                    priority="true"
-                  />
+              <Link href={`/marketing/`}>
+                <div className="border border-gra rounded-lg flex flex-col cursor-pointer">
+                  <div
+                    key={i}
+                    className="text-center bg-gray-100 h-32 rounded-t-lg"
+                  >
+                    <img
+                      src="/img/Home/Marketing/Page/HeroBackground/HeroBG.png"
+                      alt="hyaroo"
+                      width="100%"
+                      height="100%"
+                      className="rounded-md"
+                      priority="true"
+                    />
+                  </div>
+                  <div className="px-4 py-4 bg-white rounded-b-lg">
+                    <p className="mb-1 text-gray-900 text-sm font-semibold">
+                      {section.title}
+                    </p>
+                    <p className="text-gray-500 text-xs font-medium">
+                      {section.description}
+                    </p>
+                  </div>
                 </div>
-                <div className="px-4 py-4 bg-white rounded-b-lg">
-                  <p className="mb-1 text-gray-900 text-sm font-semibold">
-                    {section.title}
-                  </p>
-                  <p className="text-gray-500 text-xs font-medium">
-                    {section.description}
-                  </p>
-                </div>
-              </div>
+              </Link>
             ))}
           </div>
         </div>
---
+++ src/views/SingleSectionView/Data/index.js
@@ -0,0 +1,42 @@
+export const marketingDatas = [
+  {
+    title: "Page Section",
+    sections: [
+      { title: "Hero Sections", description: "9 components" },
+      { title: "Feature Sections", description: "10 components" },
+      { title: "CTA Sections", description: "8 components" },
+      { title: "Pricing Sections", description: "9 components" },
+      { title: "Header Sections", description: "5 components" },
+      { title: "FAQs", description: "9 components" },
+      { title: "Newsletter Sections", description: "6 components" },
+      { title: "Stats", description: "4 components" },
+      { title: "Testimonials", description: "5 components" },
+      { title: "Blog Sections", description: "3 components" },
+      { title: "Contact Sections", description: "7 components" },
+      { title: "Team Sections", description: "8 components" },
+      { title: "Content Sections", description: "6 components" },
+      { title: "Footers", description: "7 components" },
+      { title: "Logo Clouds", description: "5 components" },
+    ],
+  },
+  {
+    title: "Elements",
+    sections: [
+      { title: "Headers", description: "5 components" },
+      { title: "Banners", description: "4 components" },
+      { title: "Flyout Menus", description: "6 components" },
+    ],
+  },
+  {
+    title: "Feedback",
+    sections: [{ title: "404 Pages", description: "6 components" }],
+  },
+  {
+    title: "Page Examples",
+    sections: [
+      { title: "Landing Pages", description: "3 components" },
+      { title: "Pricing Pages", description: "6 components" },
+      { title: "Contact Pages", description: "4 components" },
+    ],
+  },
+];
