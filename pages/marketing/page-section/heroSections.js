bc5c64391e2a03ffd99633da8b37e1641e3a5060
Abid Anjum Khan <niloy.xponent@gmail.com>
2022-06-26 17:56:25 +0600

    single page preview

--- pages/marketing/page-section/heroSections.js
+++ pages/marketing/page-section/heroSections.js
@@ -1,15 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
+import Layout from "../../../src/layout";
 import FirstExample from "../../../src/views/Home/components/MarketingSection/hero/firstHero";
 export default function HeroSections() {
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 pt-16 lg:px-24">
-      <p className="text-sm text-slate-500 hover:text-slate-600 font-semibold">
-        Marketing / Page Sections
-      </p>
-      <h1 className="mt-3 text-3xl font-extrabold tracking-tight text-slate-900">
-        Hero Sections
-      </h1>
-      <FirstExample></FirstExample>
-    </div>
+    <Layout>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 pt-16 lg:px-24">
+        <p className="text-sm text-slate-500 hover:text-slate-600 font-semibold">
+          Marketing / Page Sections
+        </p>
+        <h1 className="mt-3 text-3xl font-extrabold tracking-tight text-slate-900">
+          Hero Sections
+        </h1>
+        <FirstExample />
+      </div>
+    </Layout>
   );
 }
